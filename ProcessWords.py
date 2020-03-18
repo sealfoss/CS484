62fd0b110c8571e07a9ecdbd66c634b08d4f252f
sealfoss <sealfoss@gmail.com>
2020-03-18 18:58:00 -0400

    did some stuff to process words.

--- ProcessWords.py
+++ ProcessWords.py
@@ -1,15 +1,31 @@
+import re
+from nltk.corpus import stopwords
+
+subreddit_name = "the_donald"
 in_filename = "donald_garbage.txt"
-in_file = open(in_filename, "r")
+in_file = open(in_filename, encoding="utf8")
 
 words = []
-splits = []
 lines = in_file.read().splitlines()
 lines_count = len(lines)
+stop_words = set(stopwords.words('english'))
+print("Parsing words from lines...")
 for line in lines:
-    split = list(line.split(' '))
+    reduced = re.sub(r'[^a-zA-Z\']', ' ', line)
+    split = list(reduced.split(' '))
     split = list(dict.fromkeys(split))
-    splits.append(split)
     for word in split:
-        words.append(word)
-
+        if len(word) > 1 and word not in stop_words:
+            lower = word.lower()
+            words.append(lower)
+print("Removing duplicate words...")
 words = list(dict.fromkeys(words))
+
+out_filename = subreddit_name + "_words.txt"
+out_file = open(out_filename, "w")
+
+print("Writing words to file...")
+for word in words:
+    out_file.write(word + "\n")
+print("All DONE!")
+
